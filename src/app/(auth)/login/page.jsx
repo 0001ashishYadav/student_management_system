ec2a219900b9821a4a3ffcffd96e0695cf5b8be0
000ashishYadav <000ashishyadav2003@gmail.com>
2025-06-09 00:31:39 +0530

    some work done for styling in login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 const loginPage = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-[url('https://wallpaperaccess.com/full/2314950.jpg')] flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-200">
           Sign in to your account
         </h2>
-        <p className="mt-2 text-center text-sm text-gray-600 max-w">
+        <p className="mt-2 text-center text-sm text-gray-200 max-w">
           Or
-          <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
+          <a
+            href="#"
+            className="font-medium ml-2 text-blue-600 hover:text-blue-500"
+          >
             create an account
           </a>
         </p>
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <div className="bg-white/20 md:border border-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label
-                for="email"
-                className="block text-sm font-medium text-gray-700"
-              >
+              <label className="block text-sm font-medium text-white">
                 Email address
               </label>
               <div className="mt-1">
@@ -30,19 +30,15 @@ const loginPage = () => {
                   id="email"
                   name="email"
                   type="email"
-                  autocomplete="email"
                   required
-                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-200 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Enter your email address"
                 />
               </div>
             </div>
 
             <div>
-              <label
-                for="password"
-                className="block text-sm font-medium text-gray-700"
-              >
+              <label className="block text-sm font-medium text-white">
                 Password
               </label>
               <div className="mt-1">
@@ -50,9 +46,8 @@ const loginPage = () => {
                   id="password"
                   name="password"
                   type="password"
-                  autocomplete="current-password"
                   required
-                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-200 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Enter your password"
                 />
               </div>
@@ -66,10 +61,7 @@ const loginPage = () => {
                   type="checkbox"
                   className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
                 />
-                <label
-                  for="remember_me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
+                <label className="ml-2 block text-sm text-gray-200">
                   Remember me
                 </label>
               </div>
